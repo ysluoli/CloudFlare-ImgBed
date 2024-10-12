b6f542b5af28bab9955784135019a8b0c655f6a2
ysluoli <156728360+ysluoli@users.noreply.github.com>
2024-10-12 11:38:40 +0800

    Update userConfig.js

--- functions/userConfig.js
+++ functions/userConfig.js
@@ -2,32 +2,22 @@ export async function onRequestGet(context) {
     const { request, env, params, waitUntil, next, data } = context;
     const userConfig = env.USER_CONFIG;
 
-    // 定义默认配置
-    const defaultConfig = {
-        "uploadBkImg": "bing",
-        "loginBkImg": "bing",
-        "ownerName": "Telegram",
-        "siteTitle": "Telegram 图床",
-        "bkInterval": 10000,
-        "bkOpacity": 0.8
-    };
-
-    // 如果 USER_CONFIG 为空或未定义，则使用默认配置
+    // 检查 USER_CONFIG 是否为空或未定义
     if (!userConfig) {
-        return new Response(JSON.stringify(defaultConfig), { status: 200 });
+        return new Response(JSON.stringify({}), { status: 200 });
     }
 
     try {
         // 尝试解析 USER_CONFIG 为 JSON
         const parsedConfig = JSON.parse(userConfig);
-        // 如果解析成功并且是对象，返回解析的配置；否则返回默认配置
+        // 检查解析后的结果是否为对象
         if (typeof parsedConfig === 'object' && parsedConfig !== null) {
             return new Response(JSON.stringify(parsedConfig), { status: 200 });
         } else {
-            return new Response(JSON.stringify(defaultConfig), { status: 200 });
+            return new Response(JSON.stringify({}), { status: 200 });
         }
     } catch (error) {
-        // 捕捉解析错误并返回默认配置
-        return new Response(JSON.stringify(defaultConfig), { status: 200 });
+        // 捕捉解析错误并返回空对象
+        return new Response(JSON.stringify({}), { status: 200 });
     }
 }
